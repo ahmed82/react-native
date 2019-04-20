422385e04d8bfae5df80c441399d3f72a80963d0
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2019-04-19 23:41:11 -0400

    include the Image Props basic React Native component

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      //<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.container}>
+        <Text>Hi from my son Omar!</Text>
+        <Text>I love My Family !!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a53',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
