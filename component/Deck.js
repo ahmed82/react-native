e9b23a8bd02186b560193a582baa8e400a8cff2d
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2019-05-04 19:09:54 -0400

    add rotate to the first card as Tinder flip right or left

--- component/Deck.js
+++ component/Deck.js
@@ -19,16 +19,44 @@ class Deck extends Component {
          /* there is no good resone to assign the panResponder to state */
         this.state = { panResponder, position };
     }
+
+    getCardStyle(){
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+                inputRange: [-500, 0, 500],
+                outputRange: ['-120deg', '0deg', '120deg']
+        });
+        return {
+            //...this.state.position.getLayout(), we already desetructre it above
+            ...position.getLayout(),
+            //transform: [{ rotate: '45deg'}]
+            transform: [{ rotate }] //transform: [{ rotate: rotate}]
+        };
+    }
     myRenderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            // We use the index from map to applay the animated on single card
+            if (index === 0) {
+                return (
+                    <Animated.View 
+                    key={item.id}
+                    //moved style body to helper method "getCardStyle" {this.state.position.getLayout()}
+                    style={this.getCardStyle()}
+                    
+                    {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )                
+            }
             return this.props.renderCard(item);
         });
     }
     render(){
         return ( /* use Animated.View replace View to reflect the Animatetion */
-            <Animated.View 
-                style={this.state.position.getLayout()}
-                {...this.state.panResponder.panHandlers}
+            <Animated.View
+                /* style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers} */
             >
                 {this.myRenderCards()}
             </Animated.View>
